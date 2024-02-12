c44da1505d026cecf8e47fbd9a28332911744ffb
Bestbatazaaa <Bestbatazaaa@icloud.com>
2024-02-12 22:37:19 +0700

    lab18

--- lab18_1.cpp
+++ lab18_1.cpp
@@ -5,8 +5,25 @@ struct Rect{
 	double x,y,w,h;
 };
 
-double overlap(){
+double overlap(const Rect& a, const Rect& b){
+	double Axis = a.x + a.w;
+	double Ayis = a.y - a.h;
 
+	double Bxis = b.x + b.w;
+	double Byis = b.y - b.h;
+	
+	double width = min(Axis,Bxis)-max(a.x,b.x);
+	double length = min(a.y,b.y)-max(Ayis,Byis);
+	if(width > 0 and length > 0){
+		return length*width;
+	}else{
+		return 0;
+	}
 
+}
 
+int main(){
+Rect R1 = {-1,2,6.9,9.6};
+Rect R2 = {0,0,1.2,2.5};	
+cout << overlap(R1,R2);	
 }
